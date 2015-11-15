165670c56c473f2b0966d348aafcdfb344ef0a32
Duncan Gossage <eyeamaman@gmail.com>
2015-11-15 13:04:20 +0000

    Basic setup and hitting some copy brick wall issue

---
+++ gulpfile.js
@@ -0,0 +1,25 @@
+
+'use strict';
+
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var copy = require('gulp-copy');
+
+// https://www.npmjs.com/package/gulp-sass
+gulp.task('sass', function() {
+  gulp.src('./dev/sass/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./dev/.css'));
+});
+
+gulp.task('copy', function() {
+  gulp.src(['./dev/**/*.html'])
+    .pipe(gulp.dest('./public/'));
+
+  gulp.src(['./dev/.css/*.css'])
+    .pipe(gulp.dest('./public/css'));
+});
+
+gulp.task('build', ['sass', 'copy'], function() {
+  console.log('build completed');
+});
